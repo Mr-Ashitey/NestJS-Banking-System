e200f470781ae85eb68d5666677c87e17b5309ff
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-14 16:40:56 +0000

    Add authentication credentials dto

---
+++ src/auth/dto/auth-credential.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class AuthCredentailDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+}
