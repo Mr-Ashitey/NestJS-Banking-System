d5e1f5816ba3a747a1a2ab56d7587eb20ea1fb8a
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-14 20:01:39 +0000

    Import passport, jwt and typeorm modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecretKey22',
+      signOptions: { expiresIn: 3600 }, //1 hour
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
