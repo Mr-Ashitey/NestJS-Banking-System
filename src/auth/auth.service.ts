e527a73e68a9294684ed9b48c4f8ac32ec2ebd68
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-15 14:07:25 +0000

    Add auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthCredentailDto } from './dto/auth-credential.dto';
+import { User } from './user.entity';
+// import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  // sign up service
+  //   async signUp(authCredentialsDto: AuthCredentailDto) {}
+
+  // sign in service
+  async signIn(authCredentialsDto: AuthCredentailDto) {
+    const { email, password } = authCredentialsDto;
+
+    const foundUser = await this.usersRepository.findOne({ where: { email } });
+
+    // if (foundUser && bcrypt.compareSync(password, foundUser.password)) {
+    // } else {
+    //   throw new UnauthorizedException('Invalid credentials');
+    // }
+  }
+}
