504b68820447fdc351f1e86d4f36acdededb9e25
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-15 16:40:38 +0000

    Add signup post request to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentailDto } from './dto/auth-credential.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  //   @Post('signup')
+  @Post('signup')
+  async signUp(@Body() createUserDto: CreateUserDto): Promise<object> {
+    return this.authService.signUp(createUserDto);
+  }
 
   @Post('signin')
   async signIn(@Body() authCredentialsDto: AuthCredentailDto): Promise<object> {
