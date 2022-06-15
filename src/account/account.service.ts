5d2e4b2cb55b310e2379f143563f60d20f5aab82
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-15 20:10:06 +0000

    Add create account service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,12 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from 'src/auth/user.entity';
 import { Repository } from 'typeorm';
+import { CreateAccountDto } from './dto/create-account.dto';
+import { Account } from './model/account.entity';
 
 @Injectable()
 export class AccountService {
   constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    @InjectRepository(Account)
+    private accountsRepository: Repository<Account>,
   ) {}
+
+  // create account service
+  async createAccount(createAccountDto: CreateAccountDto) {
+    const { account_name, type, next_of_kin } = createAccountDto;
+
+    const number = Math.random().toString(10).substring(2, 30); //16 numbers
+    console.log(number);
+
+    const task = this.accountsRepository.create({
+      number: 0,
+      account_name,
+      type,
+      balance: 0,
+      next_of_kin,
+      statement: '',
+      is_active: false,
+    });
+  }
 }
