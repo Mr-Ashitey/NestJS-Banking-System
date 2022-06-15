266d7881ea0f018b6c2ce154a86019701538eab1
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-15 16:46:27 +0000

    Inject user repository in account service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AccountService {}
+export class AccountService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+}
