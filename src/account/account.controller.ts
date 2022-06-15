04018e5fe1b683538d2bae8365c16e4712ccc000
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-15 20:10:57 +0000

    Add create account post endpoint to controller

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AccountService } from './account.service';
+import { CreateAccountDto } from './dto/create-account.dto';
 
 @Controller('account')
-export class AccountController {}
+export class AccountController {
+  constructor(private accountService: AccountService) {}
+
+  @Post('create-account')
+  async createAccount(@Body() createAccountDto: CreateAccountDto) {
+    return this.accountService.createAccount(createAccountDto);
+  }
+}
