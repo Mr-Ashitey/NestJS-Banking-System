7653901448b5447dfe7bdfff9419757de186a7fb
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-15 16:38:02 +0000

    Add create user dto

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,36 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsEmail,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'password too weak',
+  })
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone_no: string;
+
+  @IsString()
+  @IsNotEmpty()
+  date_of_birth: string;
+}
