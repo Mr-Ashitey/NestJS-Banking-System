8d3bd7056973253770318c427995ad253a514eb7
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-16 14:50:00 +0000

    Finish up create account controller

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,13 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 import { AccountService } from './account.service';
 import { CreateAccountDto } from './dto/create-account.dto';
+import { Account } from './model/account.entity';
 
 @Controller('account')
+@UseGuards(AuthGuard())
 export class AccountController {
+  private logger = new Logger('AccountController');
   constructor(private accountService: AccountService) {}
 
   @Post('create-account')
-  async createAccount(@Body() createAccountDto: CreateAccountDto) {
-    return this.accountService.createAccount(createAccountDto);
+  async createAccount(
+    @Body() createAccountDto: CreateAccountDto,
+    @GetUser() user: User,
+  ): Promise<object> {
+    this.logger.verbose(
+      `User "${user.userName}" creating a new task. Data: ${JSON.stringify(
+        createAccountDto,
+      )}`,
+    );
+    return this.accountService.createAccount(createAccountDto, user);
   }
 }
