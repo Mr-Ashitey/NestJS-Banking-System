c3df9f34f4d48799119ac2407b43163f9a25dfa5
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-16 14:49:33 +0000

    Import auth module so we can get the user accessing the endpoint

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { AccountController } from './account.controller';
 import { AccountService } from './account.service';
 import { Account } from './model/account.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Account])],
+  imports: [TypeOrmModule.forFeature([Account]), AuthModule],
   controllers: [AccountController],
   providers: [AccountService],
 })
