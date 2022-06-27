8f7f6d82398fb189404e2ffd234e6539920b73ca
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-27 16:14:13 +0000

    Generate verification token and send it via mail to user upon account creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import {
   ConflictException,
   Injectable,
+  InternalServerErrorException,
   Logger,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -11,11 +12,15 @@ import { AuthCredentailDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Utils } from 'src/utils';
+import * as randomstring from 'randomstring';
+
+import { Utils } from 'src/utils/utils';
+import { Mailer, MailerResponse } from 'src/utils/mailer';
 
 @Injectable()
 export class AuthService {
   logger = new Logger('AuthService');
+
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
@@ -27,27 +32,52 @@ export class AuthService {
     const { firstName, lastName, email, password, phone_no, date_of_birth } =
       createUserDto;
 
+    // hash password
     const salt = bcrypt.genSaltSync();
     const hashedPassword = bcrypt.hashSync(password, salt);
 
-    const user = this.usersRepository.create({
-      firstName,
-      lastName,
-      userName: Utils.generateUsername(firstName, lastName),
-      email,
-      password: hashedPassword,
-      phone_no,
-      date_of_birth,
-    });
+    //create verification token
+    const verificationToken = randomstring.generate();
+
+    const foundUser = await this.usersRepository.findOne({ where: { email } });
+    if (!foundUser) {
+      const mailerResponse = await new Mailer(
+        email,
+        'Verify Account Details',
+        'We have sent you a verification link at madcodein@gmail.com. Please verify account',
+      ).sendMail();
+      if (mailerResponse == MailerResponse.ERROR) {
+        throw new InternalServerErrorException(
+          'Seems there is a problem in sending you an email. Please check your connection and try again',
+        );
+      }
+    }
 
     try {
+      const user = this.usersRepository.create({
+        firstName,
+        lastName,
+        userName: Utils.generateUsername(firstName, lastName),
+        email,
+        password: hashedPassword,
+        phone_no,
+        date_of_birth,
+        verificationToken,
+      });
+
       await this.usersRepository.save(user);
 
-      return { email, success: 'User created successfully' };
+      return {
+        email,
+        success: 'User created successfully',
+        info: 'Verify your account by following the mail sent to you.',
+      };
     } catch (error) {
       throw new ConflictException(Utils.extractErrorMessage(error));
     }
   }
+
+  // account verficiation
 
   // sign in service
   async signIn(authCredentialsDto: AuthCredentailDto) {
@@ -55,6 +85,9 @@ export class AuthService {
 
     const foundUser = await this.usersRepository.findOne({ where: { email } });
 
+    if (!foundUser.active) {
+      throw new UnauthorizedException('You need to verify email first');
+    }
     if (foundUser && bcrypt.compareSync(password, foundUser.password)) {
       const payload = { email };
       const accessToken = await this.jwtService.sign(payload);
