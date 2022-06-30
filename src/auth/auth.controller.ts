fb82fedf48ff9dc742b9f1abcbef12ca08d3bd4d
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-30 10:26:33 +0000

    Add account verification endpoint to controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentailDto } from './dto/auth-credential.dto';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -10,6 +10,13 @@ export class AuthController {
   @Post('signup')
   async signUp(@Body() createUserDto: CreateUserDto): Promise<object> {
     return this.authService.signUp(createUserDto);
+  }
+
+  @Get('user-verification/:verificationToken')
+  async verifyUserAccount(
+    @Param('verificationToken') verificationToken: string,
+  ) {
+    return this.authService.verifyUserAccount(verificationToken);
   }
 
   @Post('signin')
