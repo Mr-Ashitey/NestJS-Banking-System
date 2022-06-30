621516857d7e0830a67ca9782da720f0a62a66ae
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-30 10:03:45 +0000

    Add custom function to handle email verification

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,7 @@
+import axios = require('axios');
 export class Utils {
+  private static baseUrl = 'https://emailverification.whoisxmlapi.com/api/v2';
+
   static extractErrorMessage(error: any): string {
     return error.detail
       .replace('Key', '')
@@ -15,5 +18,19 @@ export class Utils {
 
   static generateAccountNumber(): number {
     return Math.floor(10000000000000000 + Math.random() * 90000000000000000); //17 number
+  }
+
+  static async verifyEmail(email: string) {
+    try {
+      const response = await axios.default.get(
+        `${this.baseUrl}?apiKey=${process.env.EMAIL_VERIFIER_KEY}&emailAddress=${email}&checkDisposable=0&checkFree=0`,
+      );
+
+      if (!response.data.smtpCheck) {
+        throw 'Email is invalid, kindly check it and retry';
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 }
