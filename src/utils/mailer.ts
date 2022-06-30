a7c66cc781710da72f723cf2a16dd27071203e73
Emmanuel Ashitey <39267663+Mr-Ashitey@users.noreply.github.com>
2022-06-30 10:25:38 +0000

    Use html to send email instead of plain text

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -24,28 +24,94 @@ export class Mailer {
     },
   });
 
-  async sendMail(): Promise<MailerResponse> {
+  async sendMail(): Promise<any> {
     const mail_options = {
-      from: '"Banking System GH" <aamarh17@gmail.com>', // sender address
+      from: '"Banking System GH" <a@emil.com>', // sender address
       to: `${this.email}`, // list of receivers
       subject: this.subject, // Subject line
-      text: this.text, // plain text body
-      // html: '<b>Hello world?</b>', // html body
+      // text: this.text, // plain text body
+      html: `
+            <head>
+         <style>
+          table, td, div, h1, p {font-family: Arial, sans-serif;}
+        </style>
+      </head>
+      <body style="margin:0;padding:0;">
+        <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;background:#ffffff;">
+          <tr>
+            <td align="center" style="padding:0;">
+              <table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
+                <tr>
+                  <td align="center" style="padding:40px 0 30px 0;background:#848889;">
+                    <img src="https://images.unsplash.com/photo-1581349437898-cebbe9831942?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" width="300" style="height:auto;display:block;" />
+                  </td>
+                </tr>
+                <tr>
+                  <td style="padding:36px 30px 42px 30px;">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
+                      <tr>
+                        <td style="padding:0 0 36px 0;color:#153643;">
+                          <h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">Banking System GH</h1>
+                          <p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Kindly follow this link, <a href="${this.text}">ACCOUNT VERIFICATION</a> , to verify your account and activate it.</p>
+
+                        </td>
+                      </tr>
+
+                  <td style="padding:30px;background:#ee4c50;">
+                    <table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
+                      <tr>
+                        <td style="padding:0;width:50%;" align="left">
+                          <p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
+                            &reg; Banking System, Ghana 2022<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">Unsubscribe</a>
+                          </p>
+                        </td>
+                        <td style="padding:0;width:50%;" align="right">
+                          <table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
+                            <tr>
+                              <td style="padding:0 0 0 10px;width:38px;">
+                                <a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
+                              </td>
+                              <td style="padding:0 0 0 10px;width:38px;">
+                                <a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+                    </table>
+                  </td>
+                </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </body>
+      </html>
+      `, // html body
     };
 
-    const mailerResponse = await this.transporter.sendMail(mail_options);
+    try {
+      const mailerResponse = await this.transporter.sendMail(mail_options);
+      // this.transporter.sendMail(mail_options, async (err, info) => {
+      //   console.log(err);
+      //   console.log(info);
+      // });
+      // console.log(`Mailer: ${mailerResponse}`);
 
-    if (mailerResponse.rejected.length == 0) {
-      return MailerResponse.SUCCESS;
+      console.log(`Info: ${mailerResponse.accepted}`);
+      console.log(`Info: ${mailerResponse.messageId}`);
+      console.log(`Info: ${mailerResponse.pending}`);
+      console.log(`Info: ${mailerResponse.response}`);
+      console.log(`Info: ${mailerResponse.rejected}`);
+      console.log(`Info: ${mailerResponse.rejected.length}`);
+
+      if (mailerResponse.rejected.length == 0) {
+        return MailerResponse.SUCCESS;
+      }
+      return MailerResponse.ERROR;
+    } catch (error) {
+      return MailerResponse.ERROR;
     }
-    return MailerResponse.ERROR;
-
-    console.log(`Info: ${mailerResponse.accepted}`);
-    console.log(`Info: ${mailerResponse.messageId}`);
-    console.log(`Info: ${mailerResponse.pending}`);
-    console.log(`Info: ${mailerResponse.response}`);
-    console.log(`Info: ${mailerResponse.rejected}`);
-    console.log(`Info: ${mailerResponse.rejected.length}`);
   }
 
   // async sendMail(): Promise<MailerResponse> {
